4258344052ad382cfba5dc3ab7b37df7295b3944
Dorell James <galangdj@gmail.com>
2025-09-30 12:37:39 +0800

    chore: remove cookies

--- app/callback/route.ts
+++ app/callback/route.ts
@@ -1,19 +1,21 @@
 import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 import { handleSignIn } from "@logto/next/server-actions";
 import { logtoConfig } from "../logto";
 import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   await handleSignIn(logtoConfig, searchParams);
 
   // Get and clear the returnTo cookie
-  const cookieStore = cookies();
-  const returnTo = cookieStore.get('returnTo')?.value || '/';
+  // const cookieStore = await cookies();
+  // const returnTo = cookieStore.get("returnTo")?.value || "/";
 
-  // Clear the cookie after reading
-  cookieStore.delete('returnTo');
+  // // Clear the cookie after reading
+  // cookieStore.delete("returnTo");
 
-  redirect(returnTo);
+  // redirect(returnTo);
+
+  redirect("/");
 }
